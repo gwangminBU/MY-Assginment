8b702c1ee21c22ac7333e2d94c2ffbcac40687f7
gwangminBU <109594044+gwangminBU@users.noreply.github.com>
2022-07-23 12:20:56 +0900

    Assginment

---
+++ Untitled7.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #  My Jupyter Notebook on IBM Watson Studio
+
+# ### Gwang min BU
+
+# Semiconductor Manufacturing Engineer @ ETCH PROCESS
+
+# *I am interested in data science because i want to analysis process data and analysis korean stocks*
+
+# ### Total Sum of list a
+
+# In[13]:
+
+
+a = [1, 2, 3, 4, 5]
+b=0
+for n in a:
+    b = n + b 
+print(b)
+
+
+# ***
+# 
+# * list a
+# * list b
+# * list c
+# 
+# |Table | Name | Height
+# -------|------|-------
+# 1     | BU   | 175
+# 
+# My stock anlysis blog https://blog.naver.com/hhh33401
